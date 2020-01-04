a9e7a05171073c4b6dd25c536e2e1e791c05fa81
rodolfoalvesbocci <rodolfoalvesbocci@gmail.com>
2020-01-04 17:02:03 -0200

    commit inicial

---
+++ fundamentos/comentario.js
@@ -0,0 +1,8 @@
+function ConversaoGrausCelsius (celsius) {
+    
+    const resultado = (celsius * 9/5) + 32;
+    
+    console.log(resultado, 'Fº');
+}
+
+ConversaoGrausCelsius(50)
---
+++ fundamentos/variaveis.js
@@ -0,0 +1,12 @@
+const naomutavel = 'Rodolfo'
+const valor = 6
+
+let mutavel = 'Uva'
+
+mutavel = 'Banana'
+
+console.log(naomutavel)
+
+console.log(valor)
+
+console.log(mutavel)
